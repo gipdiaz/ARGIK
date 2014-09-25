d876fd9e22b5553891e073a4a0d9ceadab5c4c7d
gleschevich <gejo.leschevich@gmail.com>
2014-09-25 14:06:31 -0300

    No anda repetir gesto. Agregada ventana Configuracion

---
+++ ARGIX/Configuracion.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ARGIK
+{
+	/// <summary>
+	/// Lógica de interacción para Configuracion.xaml
+	/// </summary>
+	public partial class Configuracion : Window
+	{
+		readonly ColorStreamManager colorManager = new ColorStreamManager();
+        readonly DepthStreamManager depthManager = new DepthStreamManager();
+        SkeletonDisplayManager skeletonDisplayManager;
+		readonly ContextTracker contextTracker = new ContextTracker();
+		KinectSensor kinectSensor;
+		//Manejador de la camara
+        BindableNUICamera nuiCamera;
+
+        //Lista de esqueletos detectados
+        private Skeleton[] skeletons;
+		
+		public Configuracion()
+		{
+			this.InitializeComponent();
+			
+			// A partir de este punto se requiere la inserción de código para la creación del objeto.
+		}
+		/// <summary>
+        /// Handles the StatusChanged event of the Kinects control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="StatusChangedEventArgs" /> instance containing the event data.</param>
+        public void Kinects_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case KinectStatus.Connected:
+                    if (kinectSensor == null)
+                    {
+                        kinectSensor = e.Sensor;
+                        Initialize();
+                    }
+                    break;
+                case KinectStatus.Disconnected:
+                    if (kinectSensor == e.Sensor)
+                    {
+                        Clean();
+                        MessageBox.Show("Kinect was disconnected");
+                    }
+                    break;
+                case KinectStatus.NotReady:
+                    break;
+                case KinectStatus.NotPowered:
+                    if (kinectSensor == e.Sensor)
+                    {
+                        Clean();
+                        MessageBox.Show("Kinect is no more powered");
+                    }
+                    break;
+                default:
+                    MessageBox.Show("Unhandled Status: " + e.Status);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Inicializa el sensor
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs" /> instance containing the event data.</param>
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Controla los eventos que tienen que ver con el cambio de estado del sensor
+                KinectSensor.KinectSensors.StatusChanged += Kinects_StatusChanged;
+
+                //Busca los sensores conectados y acciona el que ya se encuentra listo
+                foreach (KinectSensor kinect in KinectSensor.KinectSensors)
+                {
+                    if (kinect.Status == KinectStatus.Connected)
+                    {
+                        kinectSensor = kinect;
+                        break;
+                    }
+                }
+                if (KinectSensor.KinectSensors.Count == 0)
+                    MessageBox.Show("No se encontro un Kinect conectado");
+                else
+                    Initialize();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Initializes this instance.
+        /// </summary>
+        public void Initialize()
+        {
+            if (kinectSensor == null)
+                return;
+
+            kinectSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+            kinectSensor.ColorFrameReady += kinectRuntime_ColorFrameReady;
+
+            kinectSensor.DepthStream.Enable(DepthImageFormat.Resolution320x240Fps30);
+            kinectSensor.DepthFrameReady += kinectSensor_DepthFrameReady;
+
+            kinectSensor.SkeletonStream.Enable(new TransformSmoothParameters
+            {
+                Smoothing = 0.5f,
+                Correction = 0.5f,
+                Prediction = 0.5f,
+                JitterRadius = 0.05f,
+                MaxDeviationRadius = 0.04f
+            });
+            kinectSensor.SkeletonFrameReady += kinectRuntime_SkeletonFrameReady;
+
+            skeletonDisplayManager = new SkeletonDisplayManager(kinectSensor, kinectCanvas);
+            
+            //Encender el sensor
+            kinectSensor.Start();
+
+            
+           
+            
+            
+        
+           
+            //Controla la elevacion de la camara con el slider de la GUI
+            nuiCamera = new BindableNUICamera(kinectSensor);
+            elevacionCamara.DataContext = nuiCamera;
+
+            //Mostrar en pantalla la imagen a color
+            kinectDisplay.DataContext = colorManager;
+
+            
+            }
+
+	}
+}
